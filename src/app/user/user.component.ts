61406711a30a5c33e0e0d42570f641ef7a54a439
fisiandroide <julio.limachi3@gmail.com>
2018-08-18 17:59:21 -0500

    my proyect user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { UserService } from './user.service';
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  providers:[UserService]
+})
+export class UserComponent implements OnInit {
+
+  constructor(private userService:UserService) { }
+
+  ngOnInit() {
+  }
+
+}
